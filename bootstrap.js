815199514d72c6569d1737cd59f956a7141c72ee
Tymoteusz Paszun <tymoteusz.paszun@telestream.net>
2024-12-27 13:34:26 +0100

    Deploy

---
+++ bootstrap.js
@@ -0,0 +1,6 @@
+import { dotnet } from '/_framework/dotnet.js'
+
+const is_browser = typeof window != "undefined";
+if (!is_browser) throw new Error(`Expected to be running in a browser`);
+
+window.dotnet = dotnet;
